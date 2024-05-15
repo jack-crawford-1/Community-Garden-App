17ef87e71a64f44cbe513570aea9e399d8b20b95
Jack Crawford <primes_unit.05@icloud.com>
2024-05-16 05:56:46 +1200

    mobile responsive styling

--- pages/locations/[id].tsx
+++ pages/locations/[id].tsx
@@ -45,42 +45,86 @@ export default function LocationPage({ location }: { location: Coords }) {
   const apiKey = process.env.NEXT_PUBLIC_MAPS_API_KEY
 
   return (
-    <>
-      <div className="flex flex-col justify-center items-center bg-gray-100">
-        <div className="flex flex-col items-center">
-          <h1 className="text-4xl m-10 font-bold">{location.address}</h1>
-          <div className="m-3">Added by User ID: {location.addedByUserId}</div>
-          <div className="text-lg w-5/6 leading-7 m-2">
-            {location.description}
+    <div className="outer container flex md:flex-row flex-col ">
+      <div className="topContainer justify-center items-center">
+        <div className="h1 text-5xl md:m-3 m-1 pt-5 pb-4 text-center">
+          {location.address}
+        </div>
+        <div className="addedby text-center">
+          Added By user id: {location.addedByUserId}
+        </div>
+        <div className="maintext text-center md:text-2xl flex-wrap md:m-10 m-6">
+          {location.description}
+        </div>
+
+        <div className="imgMapContainer flex flex-col md:flex-row md:pl-10 md:pr-10 m-0 justify-center w-screen">
+          <div className="image w-screen md:m-5 m-2 flex flex-row">
+            <Image
+              src={location.imageUrl}
+              alt={'imageurl'}
+              width={800}
+              height={800}
+              style={{ borderRadius: '0.5rem' }}
+            />
+          </div>
+          <div className="map w-full m-3 flex-row md:m-5">
+            <iframe
+              width="100%"
+              height="100%"
+              style={{ border: 0, borderRadius: '0.5rem' }}
+              loading="lazy"
+              allowFullScreen
+              src={`https://www.google.com/maps/embed/v1/view?key=${apiKey}&center=${location.lat},${location.lng}&zoom=18&maptype=roadmap`}
+            ></iframe>
           </div>
         </div>
-
-        <div className="border-4 border-gray-400 rounded-xl m-5">
-          <Image
-            src={location.imageUrl}
-            alt={'imageurl'}
-            width={400}
-            height={400}
-          />
+        <div className="flex flex-row md:m-5 ml-10 text-center md:justify-center w-1/2">
+          <div className="lat text-cente m-2">Latitude: {location.lat}</div>
+          <div className="Long text-center m-2">Longitude: {location.lng}</div>
         </div>
-        <div className="m-1">Latitude: {location.lat},</div>
-        <div className="mb-10">Longitude: {location.lng}</div>
 
-        <div className="w-4/5 m-1 border-2 border-gray-400 rounded-xl h-64">
-          <iframe
-            width="100%"
-            height="100%"
-            style={{ border: 0, borderRadius: '1rem' }}
-            loading="lazy"
-            allowFullScreen
-            src={`https://www.google.com/maps/embed/v1/view?key=${apiKey}&center=${location.lat},${location.lng}&zoom=18&maptype=roadmap`}
-          ></iframe>
+        <div className="homelink text-2xl md:hover:text-blue-300 text-blue-300 text-center m-4 ">
+          <Link href={`/locations`}>See All Locations</Link>
         </div>
       </div>
-
-      <div className="text-lg hover:text-blue-500 m-10">
-        <Link href={`/locations`}>See All Locations</Link>
-      </div>
-    </>
+    </div>
   )
 }
+
+// <>
+//       <div className="flex flex-col justify-center items-center bg-gray-100">
+//         <div className="flex flex-col items-center">
+//           <h1 className="text-4xl m-10 font-bold">{location.address}</h1>
+//           <div className="m-3">Added by User ID: {location.addedByUserId}</div>
+//           <div className="text-lg w-5/6 leading-7 m-2">
+//             {location.description}
+//           </div>
+//         </div>
+
+//         <div className="border-4 border-gray-400 rounded-xl m-5">
+//           <Image
+//             src={location.imageUrl}
+//             alt={'imageurl'}
+//             width={400}
+//             height={400}
+//           />
+//         </div>
+//         <div className="m-1">Latitude: {location.lat},</div>
+//         <div className="mb-10">Longitude: {location.lng}</div>
+
+//         <div className="w-4/5 m-1 border-2 border-gray-400 rounded-xl h-64">
+//           <iframe
+//             width="100%"
+//             height="100%"
+//             style={{ border: 0, borderRadius: '1rem' }}
+//             loading="lazy"
+//             allowFullScreen
+//             src={`https://www.google.com/maps/embed/v1/view?key=${apiKey}&center=${location.lat},${location.lng}&zoom=18&maptype=roadmap`}
+//           ></iframe>
+//         </div>
+//       </div>
+
+//       <div className="text-lg hover:text-blue-500 m-10">
+//         <Link href={`/locations`}>See All Locations</Link>
+//       </div>
+//     </>
--- pages/locations/index.tsx
+++ pages/locations/index.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router'
 import prisma from '../../src/app/components/prismaClient/prisma'
 import '../../src/app/styles/globals.css'
 import Link from 'next/link'
+import Image from 'next/image'
 interface Coords {
   id: number
   lat: string
@@ -9,6 +10,7 @@ interface Coords {
   address: string
   description: string
   addedByUserId: number
+  imageUrl: string
 }
 export async function getServerSideProps() {
   const coordinates = await prisma.coords.findMany()
@@ -81,47 +83,46 @@ export default function LocationsPage({
   const apiKey = process.env.NEXT_PUBLIC_MAPS_API_KEY
 
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col items-center p-10">
+    <div className="bg-gray-100 min-h-screen flex flex-col items-center md:p-10 p-2">
       {coordinates.map((location) => (
         <div
           key={location.id}
-          className="flex flex-col md:flex-row items-center justify-center bg-white shadow-md rounded-lg p-6 mb-6 w-3/4 p-10 md:min-h-96"
+          className="flex flex-col md:flex-row items-center justify-around bg-white shadow-md rounded-lg p-6 mb-6 md:w-3/4 p-10 md:min-h-96 w-full"
         >
           <div className="text-left md:w-1/2 md:ml-10">
             <Link href={`/locations/${location.id}`}>
               <h1 className="text-2xl font-bold">{location.address}</h1>
             </Link>
-            <p className="text-lg mt-2">{location.description}</p>
-            <div className="text-md mt-2 mb-4">
-              <span>
-                Lat: {location.lat}, Lng: {location.lng}
-              </span>
-              <br />
-              <span>Added by: {location.addedByUserId}</span>
+            <div className="m-1">
+              Added by user id: {location.addedByUserId}
             </div>
+            <p className="text-lg m-2">{location.description}</p>
+            <div className="text-md mt-2 mb-4"></div>
+          </div>
+          <div className="w-full flex flex-row ">
+            <Image
+              src={location.imageUrl}
+              alt={'imageurl'}
+              width={350}
+              height={200}
+              style={{ borderRadius: '0.5rem' }}
+            />
+          </div>
+
+          <span className=" flex  flex-row align-center m-2 md:m-5">
             <button
-              className="bg-red-500 text-white px-4 py-2 rounded mr-2"
+              className="bg-red-300 text-white px-4 py-2 rounded m-1"
               onClick={() => handleDelete(location.id)}
             >
               Delete
             </button>
             <button
-              className="bg-blue-500 text-white px-4 py-2 rounded"
+              className="bg-blue-300 text-white px-4 py-2 rounded m-1"
               onClick={() => handleEdit(location.id)}
             >
               Edit
             </button>
-          </div>
-          <div className="w-full md:w-1/2 h-72">
-            <iframe
-              width="100%"
-              height="100%"
-              style={{ border: 0, borderRadius: '0.5rem' }}
-              loading="lazy"
-              allowFullScreen={false}
-              src={`https://www.google.com/maps/embed/v1/view?key=${apiKey}&center=${location.lat},${location.lng}&zoom=18&maptype=roadmap`}
-            ></iframe>
-          </div>
+          </span>
         </div>
       ))}
     </div>
